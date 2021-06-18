97c78a0345d6660bb035e79fa5391319ae97a28e
JessicaPreuss <82731575+JessicaPreuss@users.noreply.github.com>
2021-06-18 17:20:27 -0400

    final draft

--- script.js
+++ script.js
@@ -39,10 +39,14 @@ const addSubmission = (array, newName, newScore, newDate) => {
 console.log(submissions.push(submission));
 };
 
+addSubmission();
+
 //declare a function named deleteSubmissionByIndex, parameters array and index//
 const deleteSubmissionByIndex = (array, index) => {
     submissions.splice(index,1,);
 };
+
+deleteSubmissionByIndex();
 //declare a function named deleteSubmissionByName, parameters array, name//
 
 
@@ -50,6 +54,8 @@ const deleteSubmissionByName = (array, name) => {
     const index = array.findIndex(element => element.name === name);
     console.log(submissions.splice(index,1,));
 };
+
+deleteSubmissionByName();
 
 //declare a function named editSubmission; parameters - array, index, score//
 
@@ -59,29 +65,32 @@ const editSubmission = (array, index, score) => {
 console.log(submissions);
 };
 
+editSubmission ();
 //declare a function named findSubmissionByName; parameters (array, name)//
 
 const findSubmissionByName = (array, name) => {
     const studentName = array.find(element => element.name === name);
     console.log(studentName);
 };
+
+findSubmissionByName ();
 //declare a function names findLowestScore with parameter array//
 
 function findLowestScore (array) {
     let scoreArray = array.map(a => a.score)
         scoreArray.forEach(function(score){
-            score = Math.min(...scoreArray)
-            // console.log(score)
+            score = Math.min(...scoreArray);
             const findLow = array.find(element => element.score === score);
             console.log(findLow);
-         scoreArray.length = 0;
+            scoreArray.length = 0;
     })
 };
 
+findLowestScore ();
 //declare a function named findLowestScore parameter (array)//
 
 function findAverageScore (array) {
-    let scoreArray = array.map(a => a.score)
+    let scoreArray = array.map(a => a.score);
     let sum = 0;
     let total = 0;
     for (let score of scoreArray) {
@@ -91,7 +100,20 @@ function findAverageScore (array) {
     console.log(total);
 };
 
+findAverageScore ();
+
 //declare a function named filterPassing - Parameters (array)//
 function filterPassing (array) {
-    
-}
+    const findPassing = array.filter(element => element.passed === true);
+    console.log(findPassing);
+};
+
+filterPassing ();
+
+//declare a function named filter90AndAbove//
+function filter90AndAbove (array) {
+    const find90Plus = array.filter(element => element.score >= 90 );
+    console.log(find90Plus);
+};
+
+filter90AndAbove ();
